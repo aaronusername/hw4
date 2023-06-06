047345a927d8db0285df293e9f7d50fb2f5ee535
Aaron Kim <aaron.kim1@kellogg.northwestern.edu>
2023-06-06 21:18:29 +0000

    updating

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "HI, #{@user["username"]}"
+        redirect_to "/places"
+      else
+        flash["notice"] = "FAILED. Check your password again"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "FAILED. Check your email again"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    flash["notice"] = "BYE BYE"
+    session["user_id"] = nil
+    redirect_to "/sessions/new"
   end
 end
-  
